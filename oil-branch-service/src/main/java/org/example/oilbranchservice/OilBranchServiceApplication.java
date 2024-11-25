8b3b4ae25ee4dcb9a7f077c9a7ad4418fbe62672
Ronnie King <ronnie.king4913@gmail.com>
2024-11-25 08:40:58 -0600

    this commit adds the service for the application

---
+++ oil-branch-service/src/main/java/org/example/oilbranchservice/OilBranchServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.oilbranchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OilBranchServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OilBranchServiceApplication.class, args);
+    }
+
+}
---
+++ oil-branch-service/src/test/java/org/example/oilbranchservice/OilBranchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.oilbranchservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OilBranchServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
